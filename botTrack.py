87344151abe83fa97cff2d9e005d3c829ba7838d
Gonzalo <gonziibarberis@gmail.com>
2023-01-02 03:06:34 -0300

    delete scheduler

--- botTrack.py
+++ botTrack.py
@@ -4,7 +4,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from apscheduler.schedulers.blocking import BlockingScheduler
 import time
 import telebot
 from flask import Flask, request
